d62cdbdf87dc0d52891805d134793bf1a35a6e60
GersonGmd <gersondiketama@yahoo.com>
2022-05-25 16:37:33 -0500

    List Comprehension 2

---
+++ main.py
@@ -0,0 +1,30 @@
+
+# This is one way
+
+temps = [221, 234, 340, 230]
+
+new_numbers = []
+
+for temp in temps:
+    divide = temp / 10
+    new_numbers.append(divide)
+
+
+print(new_numbers)
+
+
+# using List Comprehension
+
+new_temps = [temp / 10 for temp in temps]
+
+print(new_temps)
+
+
+# List comprehensionwith If conditional
+
+tempe = [221, 234, 340, -9999, 230]
+# the if condition will exclude the -9999 because we don't want to calculate this number for being negative
+new_tempe = [temper / 10 for temper in tempe if temper == -9999]
+
+
+print(new_tempe)
